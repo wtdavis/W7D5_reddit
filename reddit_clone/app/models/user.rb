e5713cfe7d7cc33dcd7f218131fbfe9908d89934
wtdavis <wtdavis@wesleyan.edu>
2023-02-24 13:57:06 -0500

    users view

--- reddit_clone/app/models/user.rb
+++ reddit_clone/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :username, :session_token, presence: true, uniqueness: true
-    validates :password_digest, :presence: true
+    validates :password_digest, presence: true
     validates :password, length: {minimum: 6}, allow_nil: true
     before_validation :ensure_session_token
 
@@ -46,5 +46,5 @@ class User < ApplicationRecord
 
 
 
-    
+
 end
