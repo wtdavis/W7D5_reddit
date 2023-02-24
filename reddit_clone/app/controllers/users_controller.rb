df31d466c9f2a431b46a3db4292631d4c860efe5
Samuel Kim <samuelskim420@gmail.com>
2023-02-24 18:33:39 +0000

    started user index html

--- reddit_clone/app/controllers/users_controller.rb
+++ reddit_clone/app/controllers/users_controller.rb
@@ -15,7 +15,32 @@ class UsersController < ApplicationController
         end
     end
 
-    def 
+    def new
+        @user = User.new
+        render :new
+    end
+
+    def create  
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+        else
+            flash.now[:errors] = @user.errors.full_messages
+            render :new
+        end
+
+    end
+
+    def destroy
+        if current_user == User.find(params[:id])
+            current_user.logout!
+            User.destroy(params[:id])
+        else
+            redirect_to users_url
+        end
+    end
+
+
 
     private
     def user_params
