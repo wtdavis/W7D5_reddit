581066c27a1837ab75ea2fb1659e177d4b95632a
Samuel Kim <samuelskim420@gmail.com>
2023-02-24 16:19:21 +0000

    finished app controller

--- reddit_clone/app/models/user.rb
+++ reddit_clone/app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
     validates :username, :session_token, presence: true, uniqueness: true
     validates :password_digest, :presence: true
     validates :password, length: {minimum: 6}, allow_nil: true
+    before_validation :ensure_session_token
+
+    attr_reader :password
 
     def self.find_by_credentials(username, password)
         user = User.find_by(username: username)
@@ -24,6 +27,24 @@ class User < ApplicationRecord
         end
         token
     end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token
+    end
+
+
+
 
     
 end
